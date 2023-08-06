73d8bf4790e8d803843bdfd6de1a5c5917b96b4c
katyn-sh <tanhung.huynh@protonmail.ch>
2023-08-06 15:40:45 -0500

    implement dotenv config for user/pw/database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ var roleArray = [];
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "2gD&R starlight reconcile relax",
-    database: "employeeDB"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 function runApp() {
